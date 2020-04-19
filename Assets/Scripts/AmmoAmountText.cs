b0add32bb48ee16554b49d8126985a76804cda8d
Kassanu <kassanu@gmail.com>
2020-04-19 16:00:36 -0400

    Food and Ammo count UI

---
+++ Assets/Scripts/AmmoAmountText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoAmountText : MonoBehaviour
+{
+    public Player player;
+    private TextMesh text;
+
+    void Start() {
+        this.player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        this.text = this.GetComponent<TextMesh>();
+    }
+
+    void Update()
+    {
+        this.text.text = this.player.Ammo + " / " + this.player.maxAmmo;
+    }
+}
---
+++ Assets/Scripts/FoodAmountText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodAmountText : MonoBehaviour
+{
+    public Player player;
+    private TextMesh text;
+
+    void Start()
+    {
+        this.player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        this.text = this.GetComponent<TextMesh>();
+    }
+
+    void Update()
+    {
+        this.text.text = "" + this.player.Food;
+    }
+}
