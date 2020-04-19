6f58ede6306afbea6013d3a906700127c7c30d41
Kassanu <kassanu@gmail.com>
2020-04-19 00:27:42 -0400

    Add rain

---
+++ Assets/Scripts/RainDisabler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainDisabler : MonoBehaviour
+{
+    public GameObject rain;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            this.rain.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            this.rain.SetActive(true);
+        }
+    }
+}
