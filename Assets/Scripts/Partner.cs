1420c63b908ed9f0cb782667d5023deac725aace
Kassanu <kassanu@gmail.com>
2020-04-19 20:30:19 -0400

    Add win condition

--- Assets/Scripts/Partner.cs
+++ Assets/Scripts/Partner.cs
@@ -7,12 +7,14 @@ public class Partner : MonoBehaviour
     public Player player;
     public bool playerInRange;
     public int food = 0;
+    public int givenFood = 0;
     public int health = 100;
     public float foodDropTime = 0;
     public float nextFoodDropTime = 100f;
     public float healthDropTime = 0;
     public float nextHealthDropTime = 2f;
     public GameObject tutorialText;
+    public GameObject winText;
 
     // Start is called before the first frame update
     void Start() {
@@ -23,36 +25,42 @@ public class Partner : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        if (this.health > 0) {
-            if (Input.GetKeyDown(KeyCode.E) && this.playerInRange) {
-                if (this.player.Food > 0) {
-                    this.player.Food -= 1;
-                    this.food += 1;
-                    this.player.holdUntilPressE = false;
-                    this.player.interactText.SetActive(false);
-                    this.tutorialText.SetActive(false);
-                }
-            }
-
-            if (!this.player.holdUntilPressE) {
-                if (this.food > 0) {
-                    if (this.foodDropTime < 0) {
-                        this.food--;
-                        this.foodDropTime = this.nextFoodDropTime;
-                    } else {
-                        this.foodDropTime -= Time.deltaTime;
-                    }
-                } else {
-                    if (this.healthDropTime < 0) {
-                        this.health--;
-                        this.healthDropTime = this.nextHealthDropTime;
-                    } else {
-                        this.healthDropTime -= Time.deltaTime;
+        if (this.givenFood >= 5) {
+            this.winText.SetActive(true);
+            StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.In, 0));
+        } else {
+            if (this.health > 0) {
+                if (Input.GetKeyDown(KeyCode.E) && this.playerInRange) {
+                    if (this.player.Food > 0) {
+                        this.player.Food -= 1;
+                        this.food += 1;
+                        this.givenFood += 1;
+                        this.player.holdUntilPressE = false;
+                        this.player.interactText.SetActive(false);
+                        this.tutorialText.SetActive(false);
                     }
                 }
 
-                if (this.playerInRange && this.player.Food > 0) {
-                    this.player.interactText.SetActive(true);
+                if (!this.player.holdUntilPressE) {
+                    if (this.food > 0) {
+                        if (this.foodDropTime < 0) {
+                            this.food--;
+                            this.foodDropTime = this.nextFoodDropTime;
+                        } else {
+                            this.foodDropTime -= Time.deltaTime;
+                        }
+                    } else {
+                        if (this.healthDropTime < 0) {
+                            this.health--;
+                            this.healthDropTime = this.nextHealthDropTime;
+                        } else {
+                            this.healthDropTime -= Time.deltaTime;
+                        }
+                    }
+
+                    if (this.playerInRange && this.player.Food > 0) {
+                        this.player.interactText.SetActive(true);
+                    }
                 }
             }
         }
